0a06bde70c5ed714b59d64f8473d7baf16917648
Rupasree <rupasree045@gmail.com>
2019-07-15 11:18:13 +0530

    Submission of task 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
@@ -6,7 +7,15 @@ public class Main {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConfigurationClass.class);
         Movie movie = context.getBean("movie", Movie.class);
-        System.out.println(movie.getActor().toString());
+        movie.setApplicationContext(context);
+       System.out.println(movie.getApplicationContext().getBean("movie",Movie.class).getActor().toString());
+
+        BeanFactory beanFactory=new AnnotationConfigApplicationContext(ConfigurationClass.class);
+        movie.setBeanFactory(beanFactory);
+        System.out.println(movie.getBeanFactory().getBean("movie",Movie.class).getActor().toString());
+        movie.setBeanName("movie");
+        System.out.println(movie.getApplicationContext().getBean(movie.getBeanName(),Movie.class).getActor().toString());
+
 
 
 
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,28 +1,65 @@
 package com.stackroute.domain;
 
 import com.stackroute.domain.Actor;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-public class Movie {
+public class Movie  implements ApplicationContextAware, BeanNameAware, BeanFactoryAware {
+    private Actor actor;
 
-   private Actor actor;
-
-    public Movie(Actor actor) {
-        this.actor = actor;
+    public ApplicationContext getApplicationContext() {
+        return applicationContext;
     }
+
+    public String getBeanName() {
+        return beanName;
+    }
+
+    public BeanFactory getBeanFactory() {
+        return beanFactory;
+    }
+
+    private ApplicationContext applicationContext;
+    private String beanName;
+    private BeanFactory beanFactory;
 
     public Movie() {
 
     }
 
+    public Movie(Actor actor) {
+        this.actor = actor;
+    }
 
     @Autowired
     public void setActor(Actor actor) {
         this.actor = actor;
-
     }
 
     public Actor getActor() {
         return actor;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+
+    }
+
+    @Override
+    public void setBeanName(String beanName) {
+        this.beanName = beanName;
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+
     }
 }
